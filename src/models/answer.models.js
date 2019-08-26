d0cdff94185d47f80cc800121d8b3356625013fc
AdjoaEdwin <aedwin905@gmail.com>
2019-08-26 20:58:15 +0000

    Added schema for answers

---
+++ src/models/answer.models.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const answerSchema = new mongoose.Schema(
+  {
+    description: {
+      type: String,
+      required: true
+    },
+    ticket: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "ticket",
+      required: true
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+      required: true
+    }
+  },
+  { timestamps: true }
+);
+
+export const Answer = mongoose.model("answer", answerSchema);
