6d5b9f074ec4820e9351138ed0feade2995e81da
AdjoaEdwin <aedwin905@gmail.com>
2019-08-26 15:27:02 +0000

    Added server

--- src/server.js
+++ src/server.js
@@ -2,4 +2,22 @@ import express from "express"
 import {json, urlencoded} from "body-parser";
 import cors from "cors";
 import morgan from morgan;
+import config from './config'
 
+export const app = express();
+
+// middleware here
+app.use(cors())
+app.use(json())
+app.use(urlencoded({extended: true}))
+app.use(morgan("dev"))
+
+app.get('/', (req, res) => {
+    res.write({message: "ok"})
+})
+
+export const start = () => {
+    app.listen(config.port, () => {
+        console.log(`Server on "http://localhost:${config.port}"`);
+    });
+};
